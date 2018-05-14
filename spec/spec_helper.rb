e6a9f3203f75371dfbc235c04a585cd57a11d39f
Garrett Thornburg <film42@gmail.com>
2018-05-14 16:21:05 -0400

    Increment always returns the numerical value of the counter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require "bundler/setup"
 require "pry"
 require "redis/cluster/activesupport"
+require "fakeredis/rspec"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
